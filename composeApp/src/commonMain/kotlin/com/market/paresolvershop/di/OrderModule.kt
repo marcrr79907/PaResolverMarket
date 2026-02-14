41a205194db5354a044fa753fbc9f5c3a9f3bdb6
marc <marcrr79907@gmail.com>
2026-02-14 17:08:05 -0500

    refractoring orderModule for replace 'GetCartItemsUseCase' >> 'GetOrderItemsUseCase'

--- composeApp/src/commonMain/kotlin/com/market/paresolvershop/di/OrderModule.kt
+++ composeApp/src/commonMain/kotlin/com/market/paresolvershop/di/OrderModule.kt
@@ -2,19 +2,18 @@ package com.market.paresolvershop.di
 
 import com.market.paresolvershop.data.repository.OrderRepository
 import com.market.paresolvershop.data.repository.implementations.OrderRepositoryImpl
-import com.market.paresolvershop.domain.cart.ClearCartUseCase
-import com.market.paresolvershop.domain.cart.GetCartItemsUseCase
 import com.market.paresolvershop.domain.orders.GetOrderDetailsUseCase
 import com.market.paresolvershop.domain.orders.GetOrderHistoryUseCase
+import com.market.paresolvershop.domain.orders.GetOrderItemsUseCase
 import com.market.paresolvershop.domain.orders.PlaceOrderUseCase
 import com.market.paresolvershop.domain.orders.UpdateOrderStatusUseCase
 import com.market.paresolvershop.ui.admin.OrderManagementViewModel
 import com.market.paresolvershop.ui.orders.OrderDetailViewModel
 import com.market.paresolvershop.ui.orders.OrderHistoryViewModel
 import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModelOf
-import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
 val orderModule = module {
@@ -23,8 +22,8 @@ val orderModule = module {
 
     // Uses Case
     factoryOf(::GetOrderDetailsUseCase)
+    factoryOf(::GetOrderItemsUseCase)
     factoryOf(::GetOrderHistoryUseCase)
-    factoryOf(::GetCartItemsUseCase)
     factoryOf(::PlaceOrderUseCase)
     factoryOf(::UpdateOrderStatusUseCase)
 
